4fb8f72ccd259b69987e8ec02e68e19cf9b87b83
Bikesh <bikeshchaudhary1211@gmail.com>
2022-07-19 20:56:34 +0545

    fix: push

---
+++ index.js
@@ -0,0 +1,82 @@
+console.log('aalu plus is running');
+
+// let music = new Audio('')
+let music = new Audio('');
+let winaudio = new Audio('winaudio.wav');
+let gameOverAudio = new Audio('gameover.wav');
+let audioTurn = new Audio('turnaudio.mp3');
+
+    gameOverAudio.play()
+
+
+let turn = "X"
+let gameOver = false
+
+const changeTurn = () =>{
+    return turn === "X"? "O": "X"
+}
+
+const checkWin = () =>{
+    let boxtext = document.getElementsByClassName('boxtext')
+    const wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    wins.forEach(e=>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText)&&
+        (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !=="")
+        ){
+                document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Player Won"
+                gameOver= true
+                winaudio.play()
+        }
+        
+    })
+
+}
+
+//Game Logic
+
+let boxes = document.getElementsByClassName("box");
+
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener("click",(e)=>{
+        if(boxtext.innerText ==='' ){
+            boxtext.innerText = turn;
+          turn =  changeTurn()
+            audioTurn.play()
+            checkWin();
+                if(!gameOver){
+                    document.getElementsByClassName("info")[0].innerText = "Turn For " + turn;
+                }
+        }
+    })
+})
+
+
+//reset button
+// let reset = dodument.getElementById('reset')
+
+reset.addEventListener("click",(e)=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element=>{
+        element.innerText = ""
+    })
+    gameOverAudio.play();
+
+    
+    turn ="X"
+    gameOver =false
+
+                    document.getElementsByClassName("info")[0].innerText = "Turn For"+ " " + turn;
+
+    
+})
