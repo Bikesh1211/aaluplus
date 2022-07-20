389af5af16f2ad5a9dd4085ab9a239fb7ec8ad32
Bikesh <bikeshchaudhary1211@gmail.com>
2022-07-20 15:21:06 +0545

    fix: cross bar

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ let audioTurn = new Audio('turnaudio.mp3');
 
 
 let turn = "X"
+                document.querySelector('.info').innerText = "Turn For " + turn
+
+
+let winner = "X"
+// turn = winner
 let gameOver = false
 
 // let xWin = 0
@@ -22,14 +27,14 @@ const changeTurn = () =>{
 const checkWin = () =>{
     let boxtext = document.getElementsByClassName('boxtext')
     const wins = [
-        [0,1,2,0,5,0],
-        [3,4,5,0,15,0],
-        [6,7,8,0,25,0],
-        [0,3,6,-10,15,90],
-        [1,4,7,0,15,90],
-        [2,5,8,10,15,90],
-        [0,4,8,0,15,45],
-        [2,4,6,0,15,-45]
+        [0,1,2,0,5,0,10],
+        [3,4,5,0,15,0,30],
+        [6,7,8,0,25,0,50],
+        [0,3,6,-10,15,90,30],
+        [1,4,7,0,15,90,30],
+        [2,5,8,10,15,90,30],
+        [0,4,8,0,15,45,30],
+        [2,4,6,0,15,-45,30]
     ]
 
     wins.forEach(e=>{
@@ -37,10 +42,22 @@ const checkWin = () =>{
         (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !=="")
         ){
                 document.querySelector('.info').innerText = "Player " + boxtext[e[0]].innerText + " Won"
+               winner =  boxtext[e[0]].innerText
+               console.log(winner);
                 gameOver= true
                 document.querySelector('.line').style.display = "block" 
                 document.querySelector('.line').style.width = '20vw'
                 document.querySelector('.line').style.transform = `translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`
+
+                var x = window.matchMedia("(max-width: 768px)")
+                if (x.matches) { // If media query matches
+                    document.querySelector('.line').style.width = '40vw'
+                    document.querySelector('.line').style.transform = `translate(${e[3]}vw,${e[6]}vw) rotate(${e[5]}deg)`
+
+
+                }
+
+              
                 // document.querySelector('.info').style.color = "red"
  
                     //reset jgame after win
@@ -97,14 +114,10 @@ reset.addEventListener("click",(e)=>{
     Array.from(boxtexts).forEach(element=>{
         element.innerText = ""
         document.querySelector('.line').style.width = '0'
-
+        document.getElementsByClassName("info")[0].innerText = "Turn For"+ " " + winner;
     })
     gameOverAudio.play();
-
-    
-    turn ="X"
     gameOver =false
 
-   document.getElementsByClassName("info")[0].innerText = "Turn For"+ " " + turn;
     
 })
